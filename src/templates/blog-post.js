3fabca6cdf956872eb4d8914a3a9610312adab98
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-07-10 20:45:18 -0500

    Initial Commit

---
+++ src/templates/blog-post.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+// export const query = graphql`
+//     query BlogPostTemplateQuery($id: String!) {
+//     sanityPost(_id: { eq: $id }) {
+//       title
+//       body
+//     }
+//   }
+// `
+
+const BlogPost = ({}) => {
+    return (
+        <div>BlogPost</div>
+    )
+}
+
+export default BlogPost
